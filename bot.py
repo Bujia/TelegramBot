0acd0ea65b2bc8a1c09c20d5b150ad0cbaf649ec
Bujia <guobujia@gmail.com>
2020-10-01 22:51:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,9 +13,6 @@ chat = "insert chatnumber here"
 bot = telepot.Bot(token)
 
 
-feed = feedparser.parse(url)
-feed_entries = feed.entries
-
 today = datetime.today().day
 #f-formating
 today_match = f', {today}'
@@ -27,17 +24,20 @@ for entry in feed_entries:
          words = sentence.replace(" [720p].mkv", "")
          anime = words.replace("[HorribleSubs] ", "")
          message = "Check out todays new episodes!\n" + anime
-         print(message)
          #printing the message to telegram
          #bot.sendMessage(chat, message)
          
+
 url1 = "http://fanfox.net/manga/onepunch_man/"
 #Fetching the source code
 source_code = requests.get(url1).text
 soup = BeautifulSoup(source_code, "html.parser")
 #Finding the latest chapter
 table = soup.find("div", {"class": "detail-main-list-main"}).findAll("p")
-manga_match = datetime.datetime.strftime(datetime.datetime.now()," %b %d, %Y")
-print(manga_match)
+#Fetch current date -> Month, date, year format
+date = datetime.strftime(datetime.today(), "%b %d,%Y ")
 for element in table:
-    print(element.text)
+    #Comparing scraped date to current date
+    if element.text == date:
+        bot.sendMessage(chat1, "New Onepunch-Man chapter is out!")
+
